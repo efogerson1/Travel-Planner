2f9adf8deed5af9030a226f18aeae7c2b38418cc
efogerson1 <efogerson2@gmail.com>
2023-06-08 20:55:16 -0700

    Added HTML, CSS, JS sheets, comments for order of operation in JS sheet.

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Place holding comment */
+/* 
+Order of operation:
+1. Miranda is making calls to APIs, pulling values
+2. Eric is handling call output, putting pulled information into local storage for future use
+3. Jason printing local storage to page, weather results & currecy comparisons
+4. Nicole is creating javascript logic to conditionally show cards, either all conditions are good for travel, OR one condition is not favorable, 'think about it'
+        -Nicole will be styling HTML with CSS framework other than Bootstrap (make sure page is responsive)
+
+Good luck everyone! Go team!
+*/
